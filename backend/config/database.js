62d2ec33aae41d4665a0d5757f191ee9faad029f
sushil <gpsushil@gmail.com>
2023-07-06 23:31:24 +0530

    modified

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 module.exports = configDB = async(req, res)=>{
     try{
-        const DB = await mongoose.connect('mongodb://127.0.0.1:27017/expense-management-app')
+        const DB = await mongoose.connect(process.env.MONGODB_CONNECTION)
         console.log("Connected to db")
         
     }catch(e){
