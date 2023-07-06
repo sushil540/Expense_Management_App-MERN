c25b8cb036ff46d6d934240f5fa7736ec43d35f6
sushil <gpsushil@gmail.com>
2023-07-06 23:39:18 +0530

    modified

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export default axios.create({baseURL:'http://localhost:4080'})
+export default axios.create({baseURL:'https://expense-management-app-mern-pink.vercel.app'})
